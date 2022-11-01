8f221c33b650f4315e8fe82ff84f283c26614c5a
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-11-01 20:37:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function clicker(){
     score += 1;
     document.getElementById("score").innerHTML = score;
 
-    // Change image 
     switch(score) {
         return Math.floor(Math.random() * 2);
     }         
